a5ad2883221d86cc12c3124e49032eade0eb3011
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-05-07 22:53:25 +0700

    - create browser instances - create launch browser step - add element to a step definition and interact it - create utils to add cucumber timeout - include utils to the custom script

--- src/step-definitions/home-page-steps.ts
+++ src/step-definitions/home-page-steps.ts
@@ -1,14 +1,23 @@
 import { Given, When } from '@cucumber/cucumber';
-import { Browser, Page } from 'playwright';
+import { chromium, Browser, Page } from 'playwright';
 
 let browser: Browser; // represent browser instance
 let context: any; // represent a browser context (a separate session)
 let page: Page; // represent a single tab within the context
 
 Given('I navigate to Webdriveruniversity home page', async () => {
-    console.log('step 1');
+    // Set the browser instance
+    browser = await chromium.launch({ headless: false }); // launch the browser
+    context = await browser.newContext({viewport: {width:1920, height:1080}}); // create a new browser context
+    page = await context.newPage(); // create a new page    
+
+    // navigate to the URL
+    await page.goto('https://www.webdriveruniversity.com/'); // navigate to the URL
 });
 
 When('I clik Contact Us', async () => {
-    console.log('step 2');
+    // await page.pause(); // pause the execution for debugging
+
+    const contactUs_button = await page.getByRole('link', { name: 'CONTACT US Contact Us Form' });
+    await contactUs_button.click(); // click the button
 });
