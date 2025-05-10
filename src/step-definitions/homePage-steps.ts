1e783a61eeb547f3bf5861854f88b297cfdff45d
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-05-10 23:54:19 +0700

    Refactor browser instance implement pageFixture in stepdef

--- src/step-definitions/homePage-steps.ts
+++ src/step-definitions/homePage-steps.ts
@@ -1,28 +1,35 @@
 import { Given, When, Then } from '@cucumber/cucumber';
-import { chromium, Browser, Page } from 'playwright';
+import { pageFixture } from './hooks/browserContextFixture';
 
-let browser: Browser; // represent browser instance
-let context: any; // represent a browser context (a separate session)
-let page: Page; // represent a single tab within the context
+const url = 'https://www.webdriveruniversity.com/'; // URL to navigate to
 
 Given('I navigate to Webdriveruniversity home page', async () => {
     // navigate to the URL
-    await page.goto('https://www.webdriveruniversity.com/'); // navigate to the URL
+    await pageFixture.page.goto(url); // navigate to the URL
 });
 
 When('I clik Contact Us', async () => {
     // await page.pause(); // pause the execution for debugging
 
-    const contactUs_button = await page.getByRole('link', { name: 'CONTACT US Contact Us Form' });
+    const contactUs_button = await pageFixture.page.getByRole('link', { name: 'CONTACT US Contact Us Form' });
     await contactUs_button.click(); // click the button
 });
 
 When('I switch to the new browser tab', async () => {
-    page = await context.waitForEvent('page'); // wait for the new tab to open
-    await page.bringToFront(); // bring the new tab to the front
+     // wait for the new tab to open
+    await pageFixture.context.waitForEvent('page');
+
+    // get the new tab
+    const allPages = pageFixture.context.pages();
+    // get the last page (the new tab) and assign it to pageFixture.page
+    const newPage = allPages[allPages.length - 1]; 
+    // bring the new tab to the front
+    await pageFixture.page.bringToFront(); 
+
+    await pageFixture.page.setViewportSize({ width: 1920, height: 1080 });
 });
 
-When('I type a first name', async () => {
-    await page.pause(); 
-    await page.getByPlaceholder('First Name').fill('Joe'); 
-});
+// When('I type a first name', async () => {
+//     await page.pause(); 
+//     await page.getByPlaceholder('First Name').fill('Joe'); 
+// });
