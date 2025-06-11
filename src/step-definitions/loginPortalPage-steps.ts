a9d6f16844d8656cac08eb4447793ea87faae080
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-06-11 22:15:07 +0700

    add alert handling and message assertion

--- src/step-definitions/loginPortalPage-steps.ts
+++ src/step-definitions/loginPortalPage-steps.ts
@@ -2,15 +2,26 @@ const { When, Then } = require('@cucumber/cucumber');
 import { expect } from '@playwright/test';
 import { pageFixture } from './hooks/browserContextFixture';
 
-When('I enter {string} and {string}', async (username:string, password:string) => {
+let alertText: string;
+
+When('I enter {string} and {string}', async (username: string, password: string) => {
     // Fill in the username and password fields
     await pageFixture.page.getByPlaceholder('Username').fill(username);
     await pageFixture.page.getByPlaceholder('Password').fill(password);
 });
 
-Then('I should see the {string} message', async (message: string) => {
-    const alert = await pageFixture.page.evaluate(() => {
-        return window.alert();
+When('I click on the login button', async () => {
+    // get the alert text when the dialog appears
+    await pageFixture.page.on('dialog', async (alert) => {
+        alertText = alert.message();
+        alert.accept(); // accept the alert dialog
     });
-    expect(alert).toContain(message);
+
+    const loginButton = pageFixture.page.getByRole('button', { name: 'Login' });
+    await loginButton.hover();
+    await loginButton.click({ force: true });
+});
+
+Then('I should see the {string} message', async (message: string) => {
+    expect(alertText).toBe(message);
 });
