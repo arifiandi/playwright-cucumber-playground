612a29932f4be705ba4746cc29aeb37665546dbf
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-07-16 00:00:41 +0800

    Fix error message when executing

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const common = `./src/features/*.feature \
   --require ./src/step-definitions/**/**/*.ts \
   --require ./src/utils/cucumber-timeout.ts \
   -f json:./reports/report.json \
-  --format html:./reports/report.html;`;
+  --format html:./reports/report.html`;
 
 // Define an interface for the profile object
 // It defines an interface where each key is a string and its value is a string
