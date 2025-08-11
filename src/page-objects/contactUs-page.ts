f9db8df44b40f4ac4d10d0da9986e103f862f7c0
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-08-12 07:58:23 +0800

    POM implementation: part 4 Add functions at contactUs POM: 	- getSuccessMessage 	- getHeaderText 	- getHeaderText

--- src/page-objects/contactUs-page.ts
+++ src/page-objects/contactUs-page.ts
@@ -22,4 +22,36 @@ export class ContactUsPage extends BasePage {
         await this.page.waitForSelector('input[value="SUBMIT"]');
         await this.page.getByRole('button', { name: 'SUBMIT' }).click();
     }
+
+    public async getSuccessMessage(): Promise<string> {
+        await this.page.waitForSelector('#contact_reply h1', { timeout: 60000 });
+        return await this.page.innerText('#contact_reply h1');
+
+    }
+
+    public async getErrorMessage(): Promise<string> {
+        await this.page.waitForSelector('body');
+        const bodyElement = await this.page.locator('body');
+        const bodyText = await bodyElement.textContent();
+        return bodyText ?? ''; // if the following constant is null, return an empty string
+    }
+
+    public async getHeaderText(message: string): Promise<string> {
+        // wait for target element
+        await this.page.waitForSelector('//h1 | //body', { state: 'visible' });
+
+        // get all elements 
+        const elements = await this.page.locator('//h1 | //body').elementHandles();
+        let foundElement = '';
+
+        // loop through elements and check if any of them contain the expected text
+        for (const element of elements) {
+            let text = await element.innerText();
+            if (text.includes(message)) {
+                foundElement = text;
+                break; // exit loop if found
+            }
+        }
+        return foundElement;
+    }
 }
