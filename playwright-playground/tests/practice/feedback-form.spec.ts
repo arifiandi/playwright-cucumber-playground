6e8f360fe5a757ade3e8357f0e8637e83a7b74a9
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-10-24 08:12:10 +0800

    add clear form exercise

--- playwright-playground/tests/practice/feedback-form.spec.ts
+++ playwright-playground/tests/practice/feedback-form.spec.ts
@@ -1,16 +1,21 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('FeedBackForm.html');
 });
 
-test('Submit feedback form with with required fields', async ({page}) => {
-    const nameField = page.getByRole('textbox', {name: 'Name (required):'});
-    const emailField = page.getByRole('textbox', {name: 'Email (required):'});
-    const commentField = page.getByRole('textbox', {name: 'Comment (required):'});
-    const highlightsField = page.getByRole('textbox', {name: 'Event Highlights (optional):'});
+test('Submit feedback form with with required fields', async ({ page }) => {
+
+    page.on('dialog', dialog => {
+        dialog.accept();
+    })
+
+    const nameField = page.getByRole('textbox', { name: 'Name (required):' });
+    const emailField = page.getByRole('textbox', { name: 'Email (required):' });
+    const commentField = page.getByRole('textbox', { name: 'Comment (required):' });
+    const highlightsField = page.getByRole('textbox', { name: 'Event Highlights (optional):' });
     const checkbox = page.locator('#tos');
-    const submitBtn = page.getByRole('button', {name: 'Submit'})
+    const submitBtn = page.getByRole('button', { name: 'Submit' })
 
     await nameField.fill('John Doe');
     await emailField.fill('john.doe@example.com');
@@ -20,13 +25,36 @@ test('Submit feedback form with with required fields', async ({page}) => {
 
     await submitBtn.click();
 
-    
 });
 
-test('Form is submitted with required fields - form is cleared after submit', async ({page}) => {
+test('Form is submitted with required fields - form is cleared after submit', async ({ page }) => {
+
+    page.on('dialog', dialog => {
+        dialog.accept();
+    })
+
+    const nameField = page.getByRole('textbox', { name: 'Name (required):' });
+    const emailField = page.getByRole('textbox', { name: 'Email (required):' });
+    const commentField = page.getByRole('textbox', { name: 'Comment (required):' });
+    const highlightsField = page.getByRole('textbox', { name: 'Event Highlights (optional):' });
+    const checkbox = page.locator('#tos');
+    const clearProgressBtn = page.getByRole('button', { name: 'Clear Progress' })
+
+    await nameField.fill('John Doe');
+    await emailField.fill('john.doe@example.com');
+    await commentField.fill('Great event!');
+    await highlightsField.fill('Networking opportunities');
+    await checkbox.check();
+    await clearProgressBtn.click();
+
+    await expect(nameField).toBeEmpty();
+    await expect(emailField).toBeEmpty();
+    await expect(commentField).toBeEmpty();
+    await expect(highlightsField).toBeEmpty();
+    await expect(checkbox).not.toBeChecked();
 
 });
 
-test('Form is NOT submitted without minimal fields', async ({page}) => {
+test('Form is NOT submitted without minimal fields', async ({ page }) => {
 
 });
