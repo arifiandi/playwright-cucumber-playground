243d3e0d6003a05fae2422021465d720aacdce55
Ridwan Arifiandi <ridwanarifiandi@gmail.com>
2025-10-25 19:27:30 +0800

    Exercise 2

--- playwright-playground/tests/practice/feedback-form.spec.ts
+++ playwright-playground/tests/practice/feedback-form.spec.ts
@@ -5,9 +5,10 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Submit feedback form with with required fields', async ({ page }) => {
-
+    let formsubmitted = false;
     page.on('dialog', dialog => {
         dialog.accept();
+        formsubmitted = true;
     })
 
     const nameField = page.getByRole('textbox', { name: 'Name (required):' });
@@ -25,36 +26,50 @@ test('Submit feedback form with with required fields', async ({ page }) => {
 
     await submitBtn.click();
 
+    expect(formsubmitted).toBeTruthy();
+
 });
 
 test('Form is submitted with required fields - form is cleared after submit', async ({ page }) => {
 
+    let formsubmitted = false;
+
     page.on('dialog', dialog => {
         dialog.accept();
+        formsubmitted = true;
     })
 
     const nameField = page.getByRole('textbox', { name: 'Name (required):' });
-    const emailField = page.getByRole('textbox', { name: 'Email (required):' });
     const commentField = page.getByRole('textbox', { name: 'Comment (required):' });
-    const highlightsField = page.getByRole('textbox', { name: 'Event Highlights (optional):' });
     const checkbox = page.locator('#tos');
-    const clearProgressBtn = page.getByRole('button', { name: 'Clear Progress' })
+    const submitBtn = page.getByRole('button', { name: 'Submit' })
 
     await nameField.fill('John Doe');
-    await emailField.fill('john.doe@example.com');
     await commentField.fill('Great event!');
-    await highlightsField.fill('Networking opportunities');
     await checkbox.check();
-    await clearProgressBtn.click();
 
-    await expect(nameField).toBeEmpty();
-    await expect(emailField).toBeEmpty();
-    await expect(commentField).toBeEmpty();
-    await expect(highlightsField).toBeEmpty();
-    await expect(checkbox).not.toBeChecked();
+    await submitBtn.click();
+
+    expect(formsubmitted).toBeFalsy();
 
 });
 
 test('Form is NOT submitted without minimal fields', async ({ page }) => {
+
+});
+
+test('Form is completed-clear button clears inputs', async ({ page }) => {
+
+});
+
+test('Form is completed - clear button clears memory storage', async ({ page }) => {
+    
+});
+
+test('Form is completed -clear button does not clear inputs if dialog rejected', async ({ page }) => {
+
+});
+
+test('Form is completed - save data button saves data', async ({ page }) => {
 
 });
